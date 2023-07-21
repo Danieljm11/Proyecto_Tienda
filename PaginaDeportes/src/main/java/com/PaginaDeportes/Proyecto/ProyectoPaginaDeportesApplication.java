3e9eb25845e3fbe7f7ec87813a11513b5bd5654d
Juan Diego Marin Herrera <94065036+JuanM-25@users.noreply.github.com>
2023-07-20 20:51:10 -0600

    Add files via upload
    Archivos HTML y creacion de los archivos css

---
+++ PaginaDeportes/src/main/java/com/PaginaDeportes/Proyecto/ProyectoPaginaDeportesApplication.java
@@ -0,0 +1,13 @@
+package com.PaginaDeportes.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaginaDeportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaginaDeportesApplication.class, args);
+	}
+
+}
