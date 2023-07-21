0b777e24807021949da9f3f3e3b1545ff6e7c70b
Stephanie Valverde <92193935+valverdeS7@users.noreply.github.com>
2023-07-21 00:46:24 -0600

    Creacion de los css avance semana 11
    css

---
+++ PaginaDeportes/PaginaDeportes/src/main/java/com/PaginaDeportes/Proyecto/ProyectoPaginaDeportesApplication.java
@@ -0,0 +1,18 @@
+package com.PaginaDeportes.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class ProyectoPaginaDeportesApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(ProyectoPaginaDeportesApplication.class,args);
+                }
+                
+                
+                
+          
+
+}
