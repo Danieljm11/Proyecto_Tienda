9b71b4df2ac00410b3aeecfb56557a1b0cdf1fb8
Stephanie Valverde <Stephanie Valverde@DESKTOP-STEPHANIE>
2023-08-19 23:16:01 -0600

    Avance 4

---
+++ src/test/java/com/PaginaDeportes/Proyecto/ProyectoPaginaDeportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PaginaDeportes.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaginaDeportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
